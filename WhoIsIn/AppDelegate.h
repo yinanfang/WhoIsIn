a93c38fdae336552364a33d59a77f03f5605d255
yinanfang <yinan_fang@hotmail.com>
2014-10-05 10:15:09 -0400

    Initial Commit

---
+++ WhoIsIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhoIsIn
+//
+//  Created by Golden Compass on 10/5/14.
+//  Copyright (c) 2014 Golden Compass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
